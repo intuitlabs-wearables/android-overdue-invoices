a817ddadc161e0ddd47719363b4bccaca3c0d5a0
Cynthia J Osmon <cindy_osmon@intuit.com>
2014-11-13 09:34:28 -0800

    Initial checkin of start-from-scratch Android Wear project used for creating documentation

---
+++ OverdueInvoices/app/src/main/java/overdueinvoices/example/com/overdueinvoices/GCMIntentService.java
@@ -0,0 +1,80 @@
+package overdueinvoices.example.com.overdueinvoices;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+import com.intuit.intuitwear.exceptions.IntuitWearException;
+import com.intuit.intuitwear.notifications.IWearAndroidNotificationSender;
+import com.intuit.intuitwear.notifications.IWearNotificationSender;
+import com.intuit.intuitwear.notifications.IWearNotificationType;
+import com.intuit.mobile.png.sdk.PushNotifications;
+
+
+public class GCMIntentService extends GCMBaseIntentService {
+    /**
+     * Constructor for GCMIntentService.  This class sets up the communication with
+     * the Push Notification Gateway.
+     *
+     * PROJECT_NUMBER must be your GCM Project number.  If you do not have a GCM project
+     * number, please visit https://console.developers.google.com to create an App Project.
+     */
+    public GCMIntentService() {
+        super(OverdueInvoicesActivity.PROJECT_NUMBER);
+    }
+
+    /**
+     * This callback method is invoked after a successful registration with GCM.
+     * Here we are passing the new registrationId to the PNG SDK. The SDK will
+     * send the registrationId along with any user and userGroup mappings to the
+     * PNG servers.
+     */
+    @Override
+    protected void onRegistered(Context context, String regId) {
+        PushNotifications.updateServer(context, regId);
+    }
+
+    /**
+     * This callback method is invoked when GCM delivers a notification to the
+     * device.  Here we are simply providing an example of how to display the
+     * notification to the user. There are many other implementation options.
+     * Older API versions of Android may need to use different classes and
+     * methods.
+     */
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+
+        String msgJson = intent.getStringExtra("payload");
+
+        IWearNotificationSender.Factory iWearSender = IWearNotificationSender.Factory.getsInstance();
+        IWearAndroidNotificationSender androidNotificationSender = null;
+        try {
+            androidNotificationSender = (IWearAndroidNotificationSender) iWearSender.createNotificationSender(IWearNotificationType.ANDROID, this, msgJson);
+            androidNotificationSender.sendNotification(this);
+        } catch (IntuitWearException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Callback called upon a GCM error.
+     * @param context
+     * @param arg1
+     */
+    @Override
+    protected void onError(Context context, String arg1) {
+        Log.e(TAG, "Error related to GCM: " + arg1);
+    }
+
+    /**
+     * Callback called when device is unregisterd from GCM.
+     * @param context
+     * @param arg1
+     */
+    @Override
+    protected void onUnregistered(Context context, String arg1) {
+        Log.i(TAG, "Received unregistered call");
+    }
+}
---
+++ OverdueInvoices/app/src/main/java/overdueinvoices/example/com/overdueinvoices/OverdueInvoicesActivity.java
@@ -0,0 +1,68 @@
+package overdueinvoices.example.com.overdueinvoices;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.intuit.mobile.png.sdk.PushNotifications;
+import com.intuit.mobile.png.sdk.UserTypeEnum;
+
+public class OverdueInvoicesActivity extends Activity {
+
+    /**
+     * GCM Project Number constant.
+     * Set this to your own GCM Project Number.
+     */
+    public static final String PROJECT_NUMBER = "270204895990";
+
+    /**
+     * Push Notification Gateway Sender Id constant.
+     * Set this to your own PNG Sender Id.
+     */
+    public final String pngSenderId = "b394f11c-34f0-4f14-ac97-fd96bc505d10";
+
+    /**
+     * Your QBO email address.
+     */
+    public final String username = "cosmon";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overdue_invoices);
+
+        // Set up Push Notification Gateway to receive remote notifications
+        setupPushGateway();
+    }
+
+    /*
+	 * Invoke the PushGateway SDK to register the current user to receive
+	 * notifications on their device
+	 */
+    private void setupPushGateway() {
+
+        PushNotifications.register(this, PROJECT_NUMBER,
+                username, null, UserTypeEnum.OTHER, pngSenderId,
+                false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.overdue_invoices_activiry, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
